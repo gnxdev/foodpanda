e78f66bfec3ec4b37ebe408d31f023bfa5e6ab5e
devojt2025 <dev.ojt@geniex.tech>
2025-06-30 15:03:02 +0800

    Fixed script path
    Co-authored-by: Emmanuel Mingala <emingala02@gmail.com>
    Co-authored-by: Jianella Guia Cabauatan <cabauatan.jianellaguia@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
   apps: [
     {
       name: 'fp-api-staging',
-      script: './index.js',
+      script: './backend/index.js',
       env: {
         NODE_ENV: 'staging'
       }
     },
     {
       name: 'fp-api-production',
-      script: './index.js',
+      script: './backend/index.js',
       env: {
         NODE_ENV: 'production'
       }
